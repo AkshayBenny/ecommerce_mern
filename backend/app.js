6980eca2d6fc57ac1130250f5f120ef24fb556ec
AkshayBenny <akshaybennygh@gmail.com>
2021-11-21 18:05:50 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,20 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 require("dotenv").config();
+const userRoutes = require("./routes/user"); //import routes
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+//app
+const app = express();
+
+//db
+mongoose.connect(process.env.DATABASE).then(() => {
+  console.log("Connected to database!");
 });
 
+//routes middleware
+app.use("/api", userRoutes);
 /////////////////////////////////////////
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 9000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
