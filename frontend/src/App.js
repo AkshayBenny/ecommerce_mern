9208c6a272bf680a98af62bf8f2163a42edb6f29
AkshayBenny <akshaybennygh@gmail.com>
2021-11-22 18:02:22 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import './App.css'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Hello World</h1>
+      </main>
+      <Footer />
+    </div>
+  )
 }
 
 export default App
