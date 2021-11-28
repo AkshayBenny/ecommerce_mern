23de8448a37f827006614935ad780f9ce949f43e
AkshayBenny <akshaybennygh@gmail.com>
2021-11-28 13:08:24 +0530

    update

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,16 +1,13 @@
 import express from 'express'
-import Product from '../models/productModel.js'
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productController.js'
 
 const router = express.Router()
 
-router.get('/', async (req, res) => {
-  const products = await Product.find({})
-  res.json(products)
-})
+router.route('/').get(getProducts)
 
-app.get('/:id', (req, res) => {
-  const product = products.find((item) => item._id === req.params.id)
-  res.json(product)
-})
+router.route('/:id').get(getProductById)
 
 export default router
